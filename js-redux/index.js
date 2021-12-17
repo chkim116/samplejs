4b7f589209e8e940926fe24aa1016b6b7d86fdf2
김창회 <dudnqnfqlc@naver.com>
2021-12-18 03:24:43 +0900

    prefix handle 추가

--- js-redux/index.js
+++ js-redux/index.js
@@ -3,17 +3,17 @@ import store from "./redux/store.js";
 
 const { dispatch, getState, subscribe } = store;
 
-function inCrementState() {
+function handleInCrementState() {
     dispatch({ type: INCREMENT });
     insertStateToRoot();
 }
 
-function deCrementState() {
+function handleDeCrementState() {
     dispatch({ type: DECREMENT });
     insertStateToRoot();
 }
 
-// state 등록
+// state를 HTML로
 function insertStateToRoot() {
     const root = document.getElementById("root");
     root.innerHTML = JSON.stringify(getState());
@@ -26,9 +26,9 @@ function addListener() {
         const isPlusNode = node.textContent === "+";
 
         if (isPlusNode) {
-            node.addEventListener("click", () => inCrementState());
+            node.addEventListener("click", () => handleInCrementState());
         } else {
-            node.addEventListener("click", () => deCrementState());
+            node.addEventListener("click", () => handleDeCrementState());
         }
     });
 }
--- js-redux/readme.md
+++ js-redux/readme.md
@@ -10,14 +10,14 @@ const { dispatch, getState, subscribe } = store;
 // 최초 한번 구독 선언
 subscribe();
 
-function inCrementState() {
+function handleInCrementState() {
     dispatch({ type: INCREMENT });
 }
 
-function deCrementState() {
+function handleDeCrementState() {
     dispatch({ type: DECREMENT });
 }
 
-node.addEventListener("click", () => inCrementState());
-node.addEventListener("click", () => deCrementState());
+node.addEventListener("click", () => handleInCrementState());
+node.addEventListener("click", () => handleDeCrementState());
 ```
